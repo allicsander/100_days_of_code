60b7b7d7a9a39ffa1e5203c4b7a3db69a726184e
allicsander <me@evergoal.ru>
2021-02-23 21:40:22 +0300

    swap variables

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# variables 
-name = input("What is your name?")
-print(name)
+# variables swaping
+a = input("a:")
+b = input("b:")
+
+tmp = a
+a = b
+b = tmp
+
+print("a = " + a)
+print("b = " + b)
