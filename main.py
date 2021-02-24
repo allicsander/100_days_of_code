958ebe8493d368f76c1df5ded403f67495a1715a
allicsander <me@evergoal.ru>
2021-02-24 22:37:34 +0300

    type casting example

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-num_char = len(input("What's your favourite movie?\n"))
-print("Your fav movie name has " + str(num_char) + " characters!")
+# num_char = len(input("What's your favourite movie?\n"))
+# print("Your fav movie name has " + str(num_char) + " characters!")
+
+
+print(70 + float("100.5"))
