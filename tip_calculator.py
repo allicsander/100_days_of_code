937f8f67599c551825cc4948246321c30f07a772
allicsander <me@evergoal.ru>
2021-02-25 22:38:16 +0300

    format to two decimal places

--- tip_calculator.py
+++ tip_calculator.py
@@ -5,4 +5,5 @@ tip_percentage = int(input("What percentage tip would you like to give? 10, 12, 
 people = int(input("How many people to split the bill? "))
 
 bill_per_person = (total_bill + total_bill * (tip_percentage/100))/ people
-print(f"Each person should pay: ${round(bill_per_person, 2) }")
+formatted_bill_string = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay: ${ formatted_bill_string }")
