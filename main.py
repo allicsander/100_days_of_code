ec640b4115d1cf0fc7f8be7fb857d99c6cd579db
allicsander <me@evergoal.ru>
2021-02-25 07:50:54 +0300

    f string

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 
 
-print(8//3)
-print(round(2.6666, 3))
+score = 0
+height = 1.8
+isWinning = True
+
+# f-string
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
