7ff264436ac7a0823b06d6145a34f462a16434b6
allicsander <me@evergoal.ru>
2021-02-26 21:40:48 +0300

    drain the water, it's overflow

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 
 
-age = int(input("What is your current age? "))
-
-years_remaining = 90 - age
-day_remaining = years_remaining * 365
-weeks_remaining = years_remaining * 52
-months_remaining = years_remaining * 12
-
-message = f"your have {day_remaining} days, {weeks_remaining} weeks, {months_remaining} months"
-
-print(message)
+water_level = 85
+if water_level > 80:
+    print("Drain water")
+else:
+    print("Contirue filling up the bathtub")    
