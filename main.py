262e6bce4cdb8a22094be692c73125d4b321ba44
allicsander <me@evergoal.ru>
2021-02-27 23:24:24 +0300

    add free ride for midlife crisis people

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0 
 
-# Small Pizza: $15
-# Medium Pizza: $20
-# Large Pizza: $25
-# Pepperoni for Small Pizza: +$2
-# Pepperoni for Medium or Large Pizza: +$3
-# Extra cheese for any size pizza: +$1
-
-print("Welcome to Python Pizza Deliveries!")
-
-size = input("What size pizza do you want? S, M, or L ")
-add_pepperoni = input("Do you want pepperoni? Y or N ")
-extra_cheese = input("Do you want extra cheese? Y or N ")
-
-bill = 0
-
-if size == "S":
-    bill += 15
-elif  size == "M":    
-    bill += 20
-else:
-    bill += 25
-
-if add_pepperoni == "Y":
-    if size == "S":
-        bill += 2
+if height > 120:
+    print("You can rollercoast, of course if you're bold enough")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    elif age >= 45 and age <= 55:
+        print("Everything is going to be ok. Have a free ride on us!")    
     else:
-        bill += 3    
+        bill = 12
+        print("Adult tickets are $12.")
 
-if extra_cheese == "Y":
-    bill += 1   
+    wants_photo = input("Do you want a photo taken? Mwah or Nah ")  
+    if wants_photo == "Mwah":
+        bill += 3 
+    
+    print(f"Your final bill is {bill}")
 
-print(f"Your final bill is {bill}")    
+else:
+    print("Sorry, you can't ride the rollercoast until you grow taller")
