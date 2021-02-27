a231442817152e61754da6614f657ab5f197b714
allicsander <me@evergoal.ru>
2021-02-27 15:30:36 +0300

    rollercoaster update

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ height = int(input("What is your height in cm? "))
 if height > 160:
     print("You can rollercoast, of course if you're bold enough")
     age = int(input("What is your age? "))
-    if age <= 18:
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
         print("Please pay $7.")
     else:
         print("Please pay $12.")
