13a4dba5f3418b305d67d11b762e59e26f70c3a0
allicsander <me@evergoal.ru>
2021-03-07 19:53:11 +0300

    rock paper scissors challenge completed

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -24,3 +24,43 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+import random
+
+human_choice = int(input("Type 1 for Rock, 2 for Paper or 3 for Scissors.\n"))
+
+if human_choice == 1:
+    human_gesture = rock
+elif human_choice == 2:   
+    human_gesture = paper
+elif human_choice == 3:   
+    human_gesture = scissors
+else:
+    human_gesture = "unknown gesture"        
+
+
+AI_choice = random.randint(1, 3)
+
+if AI_choice == 1:
+    AI_gesture = rock
+elif AI_choice == 2:   
+    AI_gesture = paper
+elif AI_choice == 3:   
+    AI_gesture = scissors
+
+
+print("Your choice is:")
+print(human_gesture)
+
+print("\n\nAI choice is:")
+print(AI_gesture)
+
+if human_choice == AI_choice:
+    print("It's a draw!")
+elif human_choice > 3 and human_choice < 1:   
+    print("You should choose between rock, paper and scissors, you cheater!") 
+elif (human_choice == 1 and AI_choice == 3) or \
+     (human_choice == 2 and AI_choice == 1) or \
+     (human_choice == 3 and AI_choice == 2):
+    print("You win!")  
+else:
+    print("You lose!")          
