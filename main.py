1990b6a911eb0143b67fa42f822da22bb5e62107
allicsander <me@evergoal.ru>
2021-03-07 19:19:47 +0300

    Treasure map challenge

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-# Nested lists
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
 
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
-vegetables = ["Spinach", "Kale", "Celery", "Potatoes"]
+horiontal_position = int(position[0]) -1
+vertical_position = int(position[1]) -1
 
-dirty_dozen = [fruits, vegetables]
+map[vertical_position][horiontal_position] = "X"
 
-print(dirty_dozen[1][0])
+print(f"{row1}\n{row2}\n{row3}")
