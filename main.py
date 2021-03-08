52b1cc9e2ea3b8597236a71226accf0e308d3092
allicsander <me@evergoal.ru>
2021-03-08 10:47:36 +0300

    range stuff

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 
-student_scores = input("Input the list of student scores ").split()
-
-highest_score = 0
-for score in student_scores:
-    if highest_score < int(score):
-        highest_score = int(score)
-
-print(f"The highest score in the class is {highest_score}")
+for number in range(1, 10, 3):
+    print(number)
