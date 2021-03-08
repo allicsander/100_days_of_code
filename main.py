a4cb2177aa58116d628dd30e150de06f27e35c31
allicsander <me@evergoal.ru>
2021-03-08 10:40:18 +0300

    highest score challenge

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-students_heights = input("Input a list of student heights ").split()
-for n in range(0, len(students_heights)):
-    students_heights[n] = int(students_heights[n])
 
-total_height = 0
-for height in students_heights:
-    total_height += height
-print(total_height)    
+student_scores = input("Input the list of student scores ").split()
 
-number_of_students = 0
-for height in students_heights:
-    number_of_students += 1
-print(number_of_students)   
+highest_score = 0
+for score in student_scores:
+    if highest_score < int(score):
+        highest_score = int(score)
 
-print(f"average height is {round(total_height/number_of_students)}")
-
-# short way:  sum() and len() functions
+print(f"The highest score in the class is {highest_score}")
