9245972e57090b8f6bd617ea43a504fcc2575c8f
allicsander <me@evergoal.ru>
2021-03-08 08:09:17 +0300

    loop test

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-row1 = ["⬜️","⬜️","⬜️"]
-row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️"]
-map = [row1, row2, row3]
-print(f"{row1}\n{row2}\n{row3}")
-position = input("Where do you want to put the treasure? ")
-
-horiontal_position = int(position[0]) -1
-vertical_position = int(position[1]) -1
-
-map[vertical_position][horiontal_position] = "X"
-
-print(f"{row1}\n{row2}\n{row3}")
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
