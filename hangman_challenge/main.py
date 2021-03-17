dbaa9fa9f2c8c7d70fa251c34854207ad253374d
allicsander <me@evergoal.ru>
2021-03-17 23:28:41 +0300

    hangman game update

--- hangman_challenge/main.py
+++ hangman_challenge/main.py
@@ -8,10 +8,17 @@ chosen_word = random.choice(words)
 
 print(f"Pssst, the chosen word is {chosen_word}")
 
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)    
+
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")    
+        display[position] = letter
+  
+print(display)
