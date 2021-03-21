efd02b5c2059d454344cd08162418f4e48635d07
allicsander <me@evergoal.ru>
2021-03-21 18:50:42 +0300

    paint calculator function

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 
-# def greet():
-#     print("How do you do?")
-#     print("Isn't the weather nice today?")
+import math
 
-# greet()    
-
-
-# def greet_with_name(name):
-#     print(f"hello, {name}")
-#     print(f"how do you do, {name}?")
-
-# greet_with_name('Billie')    
 def paint_calc(height, width, cover):
-    return (width * height)/cover
+    return math.ceil((width * height)/cover)
 
 test_h = int(input("heigth of the wall: "))
 test_w = int(input("width of the wall: "))
 coverage = 5
 number_of_cans = paint_calc(height=test_h, width=test_w, cover=coverage)
-print(f"Number of cans: {number_of_cans}")
+print(f"the number of cans of paint you'll need: {number_of_cans}")
