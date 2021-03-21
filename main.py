1c6f43021408b045b8a081645a4044286188349c
allicsander <me@evergoal.ru>
2021-03-21 18:24:24 +0300

    function practice

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 
 def greet():
-    print("How do you do")
-    print("Isn't the weather nice today")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
 
 greet()    
+
+
+def greet_with_name(name):
+    print(f"hello, {name}")
+    print(f"how do you do, {name}?")
+
+greet_with_name('Sasha')    
