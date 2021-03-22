eeeeb51b0ae6903e4def06f3aafd86d680660d81
allicsander <me@evergoal.ru>
2021-03-22 20:52:58 +0300

    grading program challenge

--- main.py
+++ main.py
@@ -23,13 +23,43 @@
 # for i in range(2):
 #     print(i)
 
-def count_a_words(s):
-    word_list = s.split(" ")
-    a_word_list = []
-    for word in word_list:
-        if 'а' in word or "А" in word:
-            a_word_list.append(word)      
-    return a_word_list
+# def count_a_words(s):
+#     word_list = s.split(" ")
+#     a_word_list = []
+#     for word in word_list:
+#         if 'а' in word or "А" in word:
+#             a_word_list.append(word)      
+#     return a_word_list
 
 
-print(count_a_words("настроение как дел пример новость ПАЛЬМА"))    
+# print(count_a_words("настроение как дел пример новость ПАЛЬМА"))    
+
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+for student in student_scores:
+    if student_scores[student] > 90 and student_scores[student] <= 100:
+        student_grades[student] = 'Outstanding'
+    elif  student_scores[student] > 80 and student_scores[student] <= 90:
+        student_grades[student] = 'Exceeds Expectations' 
+    elif  student_scores[student] > 70 and student_scores[student] <= 80:
+        student_grades[student] = 'Acceptable'
+    else:
+        student_grades[student] = 'Fail'
+
+print(student_grades)
+
+
+
+
+
+
+
