08486a16df684d7332b4c7cc934013e015cce26a
allicsander <me@evergoal.ru>
2021-03-23 21:20:03 +0300

    scope testng

--- main.py
+++ main.py
@@ -58,34 +58,40 @@
 # print(student_grades)
 
 
-travel_log = [
-{
-  "country": "France",
-  "visits": 12,
-  "cities": ["Paris", "Lille", "Dijon"]
-},
-{
-  "country": "Germany",
-  "visits": 5,
-  "cities": ["Berlin", "Hamburg", "Stuttgart"]
-},
-]
+# travel_log = [
+# {
+#   "country": "France",
+#   "visits": 12,
+#   "cities": ["Paris", "Lille", "Dijon"]
+# },
+# {
+#   "country": "Germany",
+#   "visits": 5,
+#   "cities": ["Berlin", "Hamburg", "Stuttgart"]
+# },
+# ]
 
 
-def add_new_country(country, visits, cities):
-    new_country={}
-    new_country["country"] = country
-    new_country["visits"] = visits
-    new_country["cities"] = cities
+# def add_new_country(country, visits, cities):
+#     new_country={}
+#     new_country["country"] = country
+#     new_country["visits"] = visits
+#     new_country["cities"] = cities
 
-    travel_log.append(new_country)
-
-
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log)
+#     travel_log.append(new_country)
 
 
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
 
+enemies = 1
+
+def increase_enemies():
+    # enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")    
 
 
 
